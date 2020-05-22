3fddbcabb4f39783dc48038e9b8e37ba25c3e6d3
kaellandrade <micael.java@hotmail.com>
2020-05-22 17:18:17 -0300

    tagsEchoPrint

---
+++ 1-tags.php
@@ -0,0 +1,20 @@
+<!-- AULA 1, 2 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 1 PHP</title>
+</head>
+<body>
+    <?php
+        echo "Utilizando a TAG padrão";
+    ?>
+    <br>
+    <?= 'Utilizando a TAG de impressão';?>
+
+    <br>
+    <? echo 'Utilizando a TAG curta'; ?>
+
+</body>
+</html>
+
