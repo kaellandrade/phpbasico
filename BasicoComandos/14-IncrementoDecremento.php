cc6bfa2ef26b43fac98c68fe1a3e197650c9e425
kaellandrade <micael.java@hotmail.com>
2020-05-28 09:24:10 -0300

    funções

---
+++ BasicoComandos/14-IncrementoDecremento.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Operadores de Incremento/decremento</title>
+    </head>
+    <body>
+        <h3>Pós-incremento</h3>
+        <?php
+            $pos = 10;
+            echo "O valor contido em a é $pos <br/>";
+            echo 'O valor contido na variável após o incremento é ' . $pos++ . ' <br/>';
+            echo "O valor atualizado é $pos";
+        ?>
+    </body>
+
+    <h3>Pré-incremento</h3>
+    <?php
+        $pre = 10;
+        echo "O valor contido em a é $pre <br/>";
+        echo 'O valor contido na variável pró o incremento é ' . ++$pre . ' <br/>';
+        echo "O valor atualizado é $pre";
+    ?>
+
+</html>
