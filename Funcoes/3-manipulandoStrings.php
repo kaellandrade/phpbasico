d7104d1ca71cbd6d9b7529729ef123747d3fbcd9
kaellandrade <micael.java@outlook.com>
2020-05-31 07:36:15 -0300

    update

--- Funcoes/3-manipulandoStrings.php
+++ Funcoes/3-manipulandoStrings.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Aula 2</title>
+        <title>2 - Manipulando Strings</title>
     </head>
 
     <body>
         <?php
-            echo 'Comando echo<br/>';
-            print ('Comando print<hr>'); 
+            
         ?>
     </body>
 </html>
