203b2766f81224e698c0c2986e5b9a7f4b405b00
kaellandrade <micael.java@outlook.com>
2020-05-31 10:07:11 -0300

    manipulando datas

--- Funcoes/4-funcoesOperacoesMatematicas.php
+++ Funcoes/4-funcoesOperacoesMatematicas.php
@@ -6,6 +6,9 @@
         <title>2 - Manipulando Strings</title>
     </head>
     <style>
+        ol{
+            list-style-type:upper-alpha;
+        }
        .funcoes li{
            font-weight:bold;
        }
@@ -20,12 +23,13 @@
         
 
         <h2>Funções Nativas Operações Matemáticas</h2>
-        <ol class='funcoes'>
+        <ol class='funcoes' >
             <li>ceil(<em>$var_aqui</em>)</li>
             <li>floor(<em>$var_aqui</em>)</li>
             <li>round(<em>$var_aqui</em>)</li>
-            <li>rand(<em>$var_aqui</em>, <em>inicio</em>, <em>fim</em>)</li>
-            <li>sqrt(<em>$var_aqui</em>, <em>procurarPor</em>, <em>substituirPor</em>)</li>
+            <li>rand(<em>inicio</em>, <em>fim</em>)</li>
+            <li>sqrt(<em>number</em>)</li>
+            <li>getrandmax()</li>
 
         </ol>
 
@@ -35,17 +39,22 @@
             echo '<hr/>';
            
             // arredonda para cima
-            echo '1---> '.ceil($number) . '<br>';
+            echo 'A. '.ceil($number) . '<br>';
 
             // arredonda para cima
-            echo '2---> '. floor($number) . '<br>';
+            echo 'B. '. floor($number) . '<br>';
 
             // arredonda com base na fração
-            echo '3---> '. round($number) . '<br>';
+            echo 'C. '. round($number) . '<br>';
 
             // gera um valor aleatório
-            echo 'Maior valor do rand: ' . '<strong>'.getrandmax().'</strong>'.'<br/>'; 
-            echo '4---> '. rand() . '<br>';
+            echo 'D. '. rand() . '<br>';
+            
+            // retorna raiz quadrada de um número
+            echo 'E. '. sqrt($number) . '<br>';
+            
+            // maior valor que o rand pode retornar
+            echo 'F. Maior valor do rand: ' . '<strong>'.getrandmax().'</strong>'.'<br/>'; 
 
 
             // string replace str_replace(<procuraPor>, <substituiPor, $string>)
---
+++ Funcoes/5-ManibulandoDatas.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>2 - Manipulando Strings</title>
+    </head>
+    <style>
+        ol{
+            list-style-type:upper-alpha;
+        }
+       .funcoes li{
+           font-weight:bold;
+       }
+           
+    </style>
+    <body>
+        
+
+        <h2>Funções Para Manipular Datas</h2>
+        <ol class='funcoes' >
+            <li>date()</li>
+            <li>date_dafault_timezone_get()</li>
+            <li>date_default_timezone_set()</li>
+            
+        </ol>
+
+        <?php
+            // configiura o timezone
+            date_default_timezone_set('America/Bahia');
+
+            // recuperação da data Atual
+            // dia/mes/ano horas:minutos
+            echo date('d/m/Y H:i');
+            echo '<br/>';
+
+            // recupera o time default da aplicação
+            echo date_default_timezone_get() . '<br/>';
+
+
+            // Calculando datas use o padrão americano
+            $data_inicial = '1996-04-08';
+            $data_final   = '2020-05-31';
+        
+            //timestamp
+            //01/01/1970 -- 2020-05-31 (js -> milissegundos / php -> segundos)
+
+            // convertendo data para segundos
+            $time_inicial = strtotime($data_inicial);
+            $time_final = strtotime($data_final);
+            $segundos_existem_dia = 24*60*60;
+    
+
+
+            echo $data_inicial . ' --> ' . $time_inicial;
+            echo '<br/>';
+            echo $data_final . ' --> ' . $time_final;
+
+            echo '<br/>';
+
+            echo "Um dia possui $segundos_existem_dia segundos.";
+            echo '<br/>';
+            $diferenca_times_segundos = abs($time_final - $time_inicial);
+            $diferenca_times_dias = $diferenca_times_segundos / $segundos_existem_dia;
+            echo "A diferença em segundos entre a data inicial e final é $diferenca_times_segundos <br/>";
+            echo "A diferença  em dias entre a data inicial e final é $diferenca_times_dias <br/>";
+
+            
+        ?>
+        
+    </body>
+</html>
