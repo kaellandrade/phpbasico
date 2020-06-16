9c8a94aaa2fdf40b8d9b648827c6512f2647669d
Micael Andrade <micael.java@outlook.com>
2020-06-15 22:06:19 -0300

    controle de perfis

--- app_help_desk/registra_chamado.php
+++ app_help_desk/registra_chamado.php
@@ -1,10 +1,12 @@
 <?php
+    session_start();
 
     foreach($_POST as $key => $value){
         $value = str_replace('#', '-', $value);
         $_POST[$key] = $value;
     }
-
+    $_POST['id'] = $_SESSION['id'];
+       
     $texto = implode('#', $_POST) . PHP_EOL; // Identifica a plataforma e adiciona um  caracter de quebra de linha
 
     $arquivo = fopen('arquivo.txt', 'a'); // abrindo um arquivo de texto
